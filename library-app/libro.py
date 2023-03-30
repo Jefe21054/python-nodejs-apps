e56eb1438e7dc9dd73b1d5ae0f8434b4191643e4
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-03-29 22:39:48 -0500

    Aplicación de gestión de biblioteca

--- library-app/libro.py
+++ library-app/libro.py
@@ -40,3 +40,38 @@ def buscar(id):
         return datos
     except mysql.Error as err:
         print('Ha ocurrido un error',err)
+
+def modificar(id,campo,nuevoValor):
+    try:
+        sentencia_sql = ''
+        con = conectar()
+        cursor = con.cursor()
+        if campo == '1':
+            sentencia_sql = 'UPDATE libro SET titulo=%s WHERE id=%s'
+        elif campo == '2':
+            sentencia_sql = 'UPDATE libro SET autor=%s WHERE id=%s'
+        elif campo == '3':
+            sentencia_sql = 'UPDATE libro SET estado=%s WHERE id=%s'
+        else:
+            print('El campo elegido no existe')
+        datos = (nuevoValor,id,)
+        cursor.execute(sentencia_sql,datos)
+        con.commit()
+        con.close()
+        msje = 'Se actualizo correctamente'
+        return msje
+    except mysql.Error as err:
+        print('Ha ocurrido un error',err)
+
+def eliminar(id):
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        sentencia_sql = 'DELETE FROM libro WHERE id=%s'
+        cursor.execute(sentencia_sql,(id,))
+        con.commit()
+        con.close()
+        msje = 'Se ha eliminado'
+        return msje
+    except mysql.Error as err:
+        print('Ha ocurrido un error',err)
--- library-app/main.py
+++ library-app/main.py
@@ -25,10 +25,10 @@ def iniciar():
                 buscar_libro()
             case '4':
                 pass
-                #modificar_libro()
+                modificar_libro()
             case '5':
                 pass
-                #eliminar_libro()
+                eliminar_libro()
             case '6':
                 break
             case _:
@@ -56,6 +56,27 @@ def buscar_libro():
     headers = ['ID','TITULO','AUTOR','ESTADO',]
     tabla = tabulate(datos,headers,tablefmt='fancy_grid')
     print(tabla)
+
+def modificar_libro():
+    id = input('Ingrese el id del libro a modificar: ')
+    nuevoValor = ''
+    respuesta = ''
+    campo = input('Seleccione el campo que desea modificar\n1. Titulo\n2. Autor\n3. Estado\n')
+    if campo == '1':
+        nuevoValor = input('Ingrese el nuevo titulo del libro: ')
+    elif campo == '2':
+        nuevoValor = input('Ingrese el nuevo autor del libro: ')
+    elif campo == '3':
+        nuevoValor = input('Ingrese el nuevo estado del libro: ')
+    else:
+        print('El campo elegido no existe')
+    respuesta = modificar(id, campo, nuevoValor)
+    print(respuesta)
+
+def eliminar_libro():
+    id = input('Ingrese el id del libro a eliminar: ')
+    respuesta = eliminar(id)
+    print(respuesta)
 
 try:
     iniciar()
